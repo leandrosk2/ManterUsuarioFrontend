ae0f5f2021d9f1a90db2b6c376ab549683dac457
leandrosk2 <leandrolima2985@gmail.com>
2018-07-04 12:29:14 -0300

    Leandro
    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ConfigService } from './services/config.service'
+import { UsuarioService } from './services/usuario.service';
+import { IndexComponent } from './index/index.component';
+import { ConsultaComponent } from './usuario/consulta/consulta.component';
+import { MenuComponent } from './menu/menu.component';
+import { CadastroComponent } from './usuario/cadastro/cadastro.component';
+import { routing } from '../app.routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    ConsultaComponent,
+    MenuComponent,
+    CadastroComponent
+
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    routing,
+    NgbModule.forRoot()
+  ],
+  providers: [ConfigService, UsuarioService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/index/index.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+})
+export class IndexComponent {
+  private mensagem: string = "Página Inicial.";
+}
